977b9c922570ac0870d89498dcb2befd964d3bb2
Gokhan Demirdoken <gokhan.demirdoken@gmail.com>
2025-07-25 15:15:41 +0200

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from openai import OpenAI
+import openai
+import tempfile   # <-- Add this line
 import os
-import tempfile
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
